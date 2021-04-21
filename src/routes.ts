b84f8bd945d2324616f2f1eb37ad42da1aa43af0
Polyana <polyanacsousa@gmail.com>
2021-04-21 15:15:21 -0300

    Criação do User e Messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import {Router} from "express";
+import { MessageController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessageController();
 
 /**
  * Tipos de parametros
@@ -13,5 +17,10 @@ const settingsController = new SettingsController();
  */
 //embuscadoproximonivel
 routes.post("/settings", settingsController.create);
+
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showbyUser);
 
 export { routes }
